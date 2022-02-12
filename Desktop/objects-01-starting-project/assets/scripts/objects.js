9633b779fd5fe1d78f28bf4a1f38ef97c3cf54a6
YevaMV <eva280988@gmail.com>
2022-02-12 23:39:25 +0400

    Rendered Elements based on Objects and filter functionality added

---
+++ Desktop/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,60 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovie = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible')
+    }
+
+    movieList.innerHTML = '';
+
+    const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+
+    filteredMovies.forEach(movie => {
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + ' - ';
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + `${key}: ${movie.info[key]}`
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl)
+    });
+}
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+        return;
+    }
+
+    const newMovie = {
+        info: {
+            title,
+            [extraName]: extraValue
+        },
+        id: Math.random()
+    };
+    
+    movies.push(newMovie);
+    renderMovie();
+};
+
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovie(filterTerm)
+}
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler)
