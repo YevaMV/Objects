a2b40ce27272a8bf5828e743f4881257c94de919
YevaMV <eva280988@gmail.com>
2022-02-13 17:18:06 +0400

    object Destructuring and call()

--- Desktop/objects-01-starting-project/assets/scripts/objects.js
+++ Desktop/objects-01-starting-project/assets/scripts/objects.js
@@ -19,10 +19,15 @@ const renderMovie = (filter = '') => {
 
     filteredMovies.forEach(movie => {
         const movieEl = document.createElement('li');
-        let text = movie.info.title + ' - ';
-        for (const key in movie.info) {
+        const { info, ...otherProps } = movie;
+        console.log(otherProps); // id
+        // const { title: movieTitle } = info;
+        let { getFormattedTitle } = movie;
+        // getFormattedTitle = getFormattedTitle.bind(movie);
+        let text = getFormattedTitle.call(movie) + ' - ';
+        for (const key in info) {
             if (key !== 'title') {
-                text = text + `${key}: ${movie.info[key]}`
+                text = text + `${key}: ${info[key]}`
             }
         }
         movieEl.textContent = text;
@@ -44,7 +49,10 @@ const addMovieHandler = () => {
             title,
             [extraName]: extraValue
         },
-        id: Math.random()
+        id: Math.random(),
+        getFormattedTitle: function() {
+            return this.info.title.toUpperCase();
+        }
     };
     
     movies.push(newMovie);
